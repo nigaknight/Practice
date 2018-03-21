a67fdbbc93db6c61e40c824024d35844c2bd65dc
nigaknight <1275647040@qq.com>
2018-03-21 16:38:39 +0800

    numpy

--- JS/Array/every.js
+++ JS/Array/every.js
@@ -1,6 +1,11 @@
-var array1=['a','b','c'];
+//every()的判断函数
+function isBelowThreshold(currentValue){
+    if(currentValue<40){
+        return true;
+    }
+}
 
-//foreach遍历数组，数组中的函数的参数就是数组中的每一个数
-array1.forEach(function(element){
-    console.log(element);
-});
+var array1=[1,2,4,32,5,21];
+
+//every()是数组的方法，遍历这个数组，判断函数都返回true时返回true
+console.log(array1.every(isBelowThreshold));
--- JS/Array/foreach.js
+++ JS/Array/foreach.js
@@ -1,11 +1,6 @@
-//every()的判断函数
-function isBelowThreshold(currentValue){
-    if(currentValue<40){
-        return true;
-    }
-}
+var array1=['a','b','c'];
 
-var array1=[1,2,4,32,5,21];
-
-//every()是数组的方法，遍历这个数组，判断函数都返回true时返回true
-console.log(array1.every(isBelowThreshold));
+//foreach遍历数组，数组中的函数的参数就是数组中的每一个数
+array1.forEach(function(element){
+    console.log(element);
+});
